eaaeaf15e862ba49cb2c76929db1ced168336c88
Adam Brunn <AdamMcAdamson@gmail.com>
2024-04-28 16:44:37 -0500

    update todo

--- main.go
+++ main.go
@@ -13,13 +13,12 @@ import (
 // Refactor game functionality to be based on GameState
 // -- Add menus (s.ActiveMenuId) for GameStates that have multiple menus (paused)
 // -- Draw
-// -- Input
 // -- Buttons (move from state.buttons to game.input/buttons) (move under menus?)
 // Buttons (play, skip turn, rotation, etc)
 // Main menu
 // Pause state
 // - LoadImageFromScreen, draw as tinted texture
-// - Draw over that menus and the like)
+// - Draw over that menus and the like
 // Load Saves in window (Use s.GameState for saves)
 // End game state and screen
 // Pick starting player
